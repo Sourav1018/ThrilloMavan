c5b00d4805ed0bb18a3bafa10f7b03e5158a7f66
inoob <inoob@MSI.bbrouter>
2023-06-21 15:43:24 +0530

    21-06-23

---
+++ Thrillo/src/test/java/webelement/UsageOfGetLocation.java
@@ -0,0 +1,18 @@
+package webelement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsageOfGetLocation {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://omayo.blogspot.com/");
+		//getLocation() -> returns Point. point is a selenium class.
+		//used to get the relative position of an element where it is rendered on the web page. 
+		Point textField = driver.findElement(By.id("ta1")).getLocation();
+		System.out.println(textField);
+		driver.quit();		
+	}
+}
---
+++ Thrillo/src/test/java/webelement/UsageOfGetRect.java
@@ -0,0 +1,23 @@
+package webelement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsageOfGetRect {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://omayo.blogspot.com/");
+		WebElement el = driver.findElement(By.id("radio1"));
+		//getRect() -> returns rectangle. Rectangle is a selenium class.
+		//its replacement of getLocation() and getSize() method.
+		//its help us to find x,y coordinate of an element and also height,width of an element
+		Rectangle e = el.getRect();
+		System.out.println("get X axies : " + e.x + ".." + e.getX());
+		System.out.println("get Y axies : " + e.y + ".." + e.getY());
+		System.out.println("get height : " + e.height + ".." + e.getHeight());
+		System.out.println("get width : " + e.width + ".." + e.getWidth());
+	}
+}
